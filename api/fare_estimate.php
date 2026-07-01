0e4a233b0d3d699aa9a58c168eb1fb4b56449390
Progga Paromita <proggaparomitaarish@email.com>
2026-07-01 20:40:11 +0600

    RSRSCBMS-121: Apply peak hour dynamic fare multipliers

--- api/fare_estimate.php
+++ api/fare_estimate.php
@@ -13,11 +13,13 @@ if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
 
 
 
+
 $distanceKm = isset($_POST['distance_km']) ? floatval($_POST['distance_km']) : 0.00;
 
 if ($distanceKm <= 0) {
     jsonResponse(['success' => false, 'message' => 'Distance must be greater than zero.'], 400);
 }
+
 
 
 
@@ -44,9 +46,11 @@ try {
 
 
 
+
 } catch (PDOException $e) {
     error_log("Peak multiplier error: " . $e->getMessage());
 }
+
 
 
 
@@ -62,6 +66,11 @@ jsonResponse([
     'rate_per_km' => RATE_PER_KM
 ]);
   
+
+
+
+
+
 
 
 
