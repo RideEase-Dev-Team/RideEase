7e9938044c4e16dbe0ab915ebfc2524950a49f81
Progga Paromita <proggaparomitaarish@email.com>
2026-07-03 22:45:00 +0600

    RSRSCBMS-139: Add ride completed views prompting fare payments

--- passenger/track_ride.php
+++ passenger/track_ride.php
@@ -41,6 +41,7 @@ if (!$rideId) {
 
 
 
+
 // Fetch ride status
 try {
     $stmt = $db->prepare("
@@ -82,10 +83,12 @@ try {
 
 
 
+
 } catch (PDOException $e) {
     setFlash('danger', "Database error loading ride tracker.");
     redirect('/passenger/dashboard.php');
 }
+
 
 
 
@@ -156,7 +159,9 @@ if (isset($_POST['action_cancel'])) {
 
 
 
+
 }
+
 
 
 
@@ -235,6 +240,7 @@ if (isset($_POST['submit_review'])) {
 
 
 
+
     }
 
 
@@ -259,7 +265,9 @@ if (isset($_POST['submit_review'])) {
 
 
 
+
 }
+
 
 
 
@@ -488,6 +496,11 @@ require_once __DIR__ . '/../includes/header.php';
 <script src="<?php echo BASE_URL; ?>/assets/js/booking.js"></script>
 <?php require_once __DIR__ . '/../includes/footer.php'; ?>
        
+
+
+
+
+
 
 
 
