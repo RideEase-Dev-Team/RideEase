fb60b0e1a3724a972450c879a73a553b30c238f1
Progga Paromita <proggaparomitaarish@email.com>
2026-07-04 20:35:00 +0600

    RSRSCBMS-163: Automatically update the passenger tracking page when matched

--- passenger/track_ride.php
+++ passenger/track_ride.php
@@ -45,6 +45,7 @@ if (!$rideId) {
 
 
 
+
 // Fetch ride status
 try {
     $stmt = $db->prepare("
@@ -90,10 +91,12 @@ try {
 
 
 
+
 } catch (PDOException $e) {
     setFlash('danger', "Database error loading ride tracker.");
     redirect('/passenger/dashboard.php');
 }
+
 
 
 
@@ -172,7 +175,9 @@ if (isset($_POST['action_cancel'])) {
 
 
 
+
 }
+
 
 
 
@@ -259,6 +264,7 @@ if (isset($_POST['submit_review'])) {
 
 
 
+
     }
 
 
@@ -287,7 +293,9 @@ if (isset($_POST['submit_review'])) {
 
 
 
+
 }
+
 
 
 
@@ -520,6 +528,11 @@ require_once __DIR__ . '/../includes/header.php';
 <script src="<?php echo BASE_URL; ?>/assets/js/booking.js"></script>
 <?php require_once __DIR__ . '/../includes/footer.php'; ?>
        
+
+
+
+
+
 
 
 
