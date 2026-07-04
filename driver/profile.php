4360c4e9fa60876b7e625b20169b3b9d8202f49a
Progga Paromita <proggaparomitaarish@email.com>
2026-07-04 17:35:00 +0600

    RSRSCBMS-148: Save driver profile image path updates

--- driver/profile.php
+++ driver/profile.php
@@ -43,6 +43,7 @@ if (isset($_POST['update_profile'])) {
 
 
 
+
     }
 
 
@@ -56,7 +57,9 @@ if (isset($_POST['update_profile'])) {
 
 
 
+
 }
+
 
 
 
@@ -87,6 +90,7 @@ try {
 } catch (PDOException $e) {
     die("Database fetch error for profile fields.");
 }
+
 
 
 
@@ -197,6 +201,11 @@ require_once __DIR__ . '/../includes/header.php';
 
 <?php require_once __DIR__ . '/../includes/footer.php'; ?>
    
+
+
+
+
+
 
 
 
