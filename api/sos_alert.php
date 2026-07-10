baae69d6bfee8cb325ae1bb1dbb0688b23ffc4b5
Progga Paromita <proggaparomitaarish@email.com>
2026-07-10 18:20:00 +0600

    RSRSCBMS-50: Story - Build emergency distress alert creation endpoint

--- api/sos_alert.php
+++ api/sos_alert.php
@@ -19,6 +19,7 @@ if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
 
 
 
+
 $rideId = !empty($_POST['ride_id']) ? intval($_POST['ride_id']) : null;
 $message = sanitize($_POST['message'] ?? 'SOS Triggered by User');
 $userId = currentUserId();
@@ -40,6 +41,12 @@ try {
 } catch (PDOException $e) {
     jsonResponse(['success' => false, 'message' => 'Failed to log SOS alert: ' . $e->getMessage()], 500);
 }
+
+
+
+
+
+
 
 
 
